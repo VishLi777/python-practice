dfc78a8304f9b0c475627c7c09707f2272afb03a
VishLi777 <70567646+VishLi777@users.noreply.github.com>
2021-04-26 10:05:37 +0300

    Add files via upload

---
+++ bonus_practice3/pack/jsonfio.py
@@ -0,0 +1,22 @@
+{
+  "1": {
+    "name": "Aram",
+    "surname": "Zam",
+    "age": 19
+  },
+  "2": {
+    "name": "Veniamin",
+    "surname": "Sheva",
+    "age": 20
+  },
+  "3": {
+    "name": "Daniil",
+    "surname": "Sych",
+    "age": 18
+  },
+  "4": {
+    "name": "Vova",
+    "surname": "Kolo",
+    "age": 41
+  }
+}
